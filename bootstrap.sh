204df2e15359b52087975af170b707eb9f9eb56a
mattwoberts <roberts.mattroberts@gmail.com>
2021-11-23 10:37:51 +0000

    Added the custom ohmyzsh stuff

---
+++ bootstrap.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+ln -s /Users/matt1/dev/dotfiles/ohmyzsh-custom.zsh /Users/matt1/.oh-my-zsh/custom/ohmyzsh-custom.zsh
---
+++ ohmyzsh-custom.zsh
@@ -0,0 +1,19 @@
+# You can put files here to add functionality separated per file, which
+# will be ignored by git.
+# Files on the custom/ directory will be automatically loaded by the init
+# script, in alphabetical order.
+
+# For example: add yourself some shortcuts to projects you often work on.
+#
+# brainstormr=~/Projects/development/planetargon/brainstormr
+# cd $brainstormr
+#
+#
+alias l='exa -lah --git'
+
+alias core='cd ~/dev/Coretastic/'
+alias ttswap='~/timetasticswap.sh'
+alias ls="exa --long --header --git --icons"
+alias lsd="exa --long --header --git --icons --sort modified"
+alias tree="ll --tree --level=4 -a -I=.git --git-ignore"
+alias zconfig="nvim ~/.oh-my-zsh/custom/matt.zsh"
